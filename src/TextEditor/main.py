414938b8af6a7c85ba79dd8666ab175223833849
KatsumiKougen <lesanonyme1001@gmail.com>
2023-02-11 22:13:45 +0700

    Add a working clock in the upper-right corner of the program

--- src/TextEditor/main.py
+++ src/TextEditor/main.py
@@ -1,23 +1,54 @@
-from dataclasses import dataclass
 from PyQt5 import QtWidgets, QtCore, QtGui
 from TextEditorUI import Ui_MainWindow
-import sys
+import sys, time
+from typing import Union
+from datetime import datetime
+
+class TE_TimeUpdate(QtCore.QThread):
+    
+    time = QtCore.pyqtSignal(str)
+    
+    def __init__(self):
+        super().__init__()
+    
+    def run(self):
+        while True:
+            cur = datetime.now().strftime("%H:%M:%S")
+            self.time.emit(cur)
+            time.sleep(.5)
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     
-    @dataclass
-    class _AppVariables:
-        WindowTitle: str = "The text editor - Untitled"
+    class _TE_AppVariables:
+    
+        WindowTitle: str = "The text editor - $file"
         
-        TextEdInsertMode: str = "`INSERT`"
-        TextEdOverwriteMode: str = "`OVERWRITE`"
+        TextEdInsertMode: str = "`I`"
+        TextEdOverwriteMode: str = "`O`"
         FileStatusLabel: str = "File: **$file**"
+        
+        CurrentWorkspaceName: list[Union[str, bool]] = [
+            ["Untitled", False]
+        ]
     
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         
-        self.setWindowTitle(self._AppVariables.WindowTitle)
+        self.TE_DisplayTitle(self._TE_AppVariables.CurrentWorkspaceName[0])
+        
+        self.TE_UpdateTimeInBackground()
+    
+    def TE_DisplayTitle(self, workspace: Union[str, bool]):
+        self.setWindowTitle(self._TE_AppVariables.WindowTitle.replace("$file", f"{workspace[0]}{'*' if not workspace[1] else ''}"))
+    
+    def TE_DisplayTime(self, arg):
+        self.Status_TimeLabel.setText(arg)
+    
+    def TE_UpdateTimeInBackground(self):
+        self._updater = TE_TimeUpdate()
+        self._updater.time.connect(self.TE_DisplayTime)
+        self._updater.start()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
