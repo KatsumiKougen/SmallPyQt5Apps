48fccbdd83105002f70e667f3293c7f2e1f984e0
KatsumiKougen <lesanonyme1001@gmail.com>
2023-02-12 21:39:48 +0700

    Adjust default tab size to "4"

--- src/TextEditor/main.py
+++ src/TextEditor/main.py
@@ -38,9 +38,13 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.TE_DisplayTitle(self._TE_AppVariables.CurrentWorkspaceName[0])
         self.TE_UpdateTimeInBackground()
         self.TE_ShowDirectory()
+        
+        self.TE_SetIndentationSpace(4)
     
     def TE_DisplayTitle(self, workspace: Union[str, bool]):
         self.setWindowTitle(self._TE_AppVariables.WindowTitle.replace("$file", f"{workspace[0]}{'*' if not workspace[1] else ''}"))
+    
+    # Functions for displaying time (HH:MM:SS)
     
     def TE_DisplayTime(self, arg):
         self.Status_TimeLabel.setText(arg)
@@ -49,6 +53,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self._updater = TE_TimeUpdate()
         self._updater.time.connect(self.TE_DisplayTime)
         self._updater.start()
+    
+    # Functions for working with directories
     
     def TE_ShowDirectory(self, directory: str = QtCore.QDir.current()):
         self.CWD_DirInput.setText(directory.dirName())
@@ -62,6 +68,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.FileView_TableWidget.setItem(idx, 3, QtWidgets.QTableWidgetItem(str(file.size())))
             self.FileView_TableWidget.setItem(idx, 4, QtWidgets.QTableWidgetItem(file.owner()))
             self.FileView_TableWidget.setItem(idx, 5, QtWidgets.QTableWidgetItem(str(file.ownerId())))
+    
+    # Functions for plain text widget
+    
+    def TE_SetIndentationSpace(self, width: int):
+        Font = self.TextEditor_MainWidget.font()
+        FontMetrics = QtGui.QFontMetricsF(Font)
+        WhitespaceWidth = FontMetrics.width(" ")
+        self.TextEditor_MainWidget.setTabStopDistance(WhitespaceWidth*width)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
