428d09548f12c33068562ea28753cd0d7ab856af
Katsumi Kougen <lesanonyme1001@gmail.com>
2023-07-22 22:24:50 +0700

    Add file "about/About.py"

--- src/TextEditor/about/About.py
+++ src/TextEditor/about/About.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 from PyQt5 import QtWidgets, QtCore, QtGui
 
 from ui.AboutDialogUI import Ui_Dialog
@@ -8,3 +8,7 @@ class TE_AboutDialog(QtWidgets.QDialog, Ui_Dialog):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        FanartPixmap = QtGui.QPixmap(f"{os.getcwd()}/img/bunny_suit_reimu.jpg")
+        FanartPixmap = FanartPixmap.scaledToWidth(200, mode=QtCore.Qt.SmoothTransformation)
+        self.AboutDialog_Image.setPixmap(FanartPixmap)
+        self.resize(FanartPixmap.width(), FanartPixmap.height())
