0846810238e8dc999263b46c23e62888d1ff6d89
Katsumi Kougen <lesanonyme1001@gmail.com>
2024-07-03 18:41:09 +0700

    Fix bugs

--- src/ColourPickerGame/colour_picker.py
+++ src/ColourPickerGame/colour_picker.py
@@ -2,7 +2,7 @@ from PyQt5 import QtWidgets, QtCore, QtGui
 from ui.CPG_ColourPickerUi import Ui_CPG_CP_IncompleteWidget
 import sys
 
-class CPG_ColourPickerWidget(Ui_CPG_CP_IncompleteWidget):
+class CPG_ColourPickerWidget(Ui_CPG_CP_IncompleteWidget, QtWidgets.QWidget):
     
     def __init__(self):
         super().__init__()
@@ -10,22 +10,23 @@ class CPG_ColourPickerWidget(Ui_CPG_CP_IncompleteWidget):
         self._CPG_CP_BuildUi(self.CPG_CP_ColourRect)
     
     def _CPG_CP_BuildUi(self, widget: QtWidgets.QLabel):
-        WidgetWidth, WidgetHeight = widget.width(), widget.height()
+        WidgetWidth, WidgetHeight = widget.width()*2, widget.height()*4
         self.CPG_CP_ColourRectPixmap = QtGui.QPixmap(WidgetWidth, WidgetHeight)
         widget.setPixmap(self.CPG_CP_ColourRectPixmap)
         self.CPG_CP_ColourRectPainter = QtGui.QPainter(widget.pixmap())
         self.CPG_CP_ColourRectPen = QtGui.QPen()
-        self.CPG_CP_ColourRectPainter.setPen(CPG_CP_ColourRectPen)
+        self.CPG_CP_ColourRectPainter.setPen(self.CPG_CP_ColourRectPen)
         
         for x in range(WidgetWidth):
             for y in range(WidgetHeight):
-                CurrentColour = QtGui.QColor.fromHsv(x*360//WidgetWidth, y*255//WidgetHeight, 255)
+                CurrentColour = QtGui.QColor.fromHsv(0, 255-y//WidgetWidth*255, 255-y//WidgetWidth*255)
                 self.CPG_CP_ColourRectPen.setColor(CurrentColour)
-                self.CPG_CP_ColourRectPainter.setPen(CPG_CP_ColourRectPen)
+                self.CPG_CP_ColourRectPainter.setPen(self.CPG_CP_ColourRectPen)
                 self.CPG_CP_ColourRectPainter.drawPoint(x, y)
         self.CPG_CP_ColourRectPainter.end()
 
 if __name__ == "__main__":
     App = QtWidgets.QApplication(sys.argv)
     Window = CPG_ColourPickerWidget()
+    Window.show()
     sys.exit(App.exec())
