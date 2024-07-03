faf87fed02fcbdc98fbde6d664c15596266b91b2
Katsumi Kougen <lesanonyme1001@gmail.com>
2024-07-03 10:58:00 +0700

    Make changes to "main.py"

--- src/ColourPickerGame/main.py
+++ src/ColourPickerGame/main.py
@@ -1,1 +1,14 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
+from ui.CPG_MainUi import CPG_MainWindow
+import sys
+
+class Main:
+    
+    def __init__(self):
+        self.App = QtWidgets.QApplication(sys.argv)
+        self.MainWindow = CPG_MainWindow(800, 600, "The Colour Picker Game")
+        self.MainWindow.show()
+        sys.exit(self.App.exec())
+
+if __name__ == "__main__":
+    a = Main()
